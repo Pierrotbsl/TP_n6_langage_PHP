526fa3cd37e6bd9e55fe35dedf8f021fd2987ebe
Pierre Boislève <pierre.boisleve@gmail.com>
2020-03-24 16:25:04 +0100

    Initial Commit

---
+++ Formulaire.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class Formulaire
+{
+    public function __construct($_nom, $methode)
+    {
+        echo "<form action=\"\" method=$methode></form>";
+    }
+    public function ajouterzonetexte($nom)
+    {
+       echo "Votre".$nom.": <input type=\"text\" id=$nom name=\"user_\".$nom >";
+       echo "\n";
+    }
+    public function ajouterbouton()
+    {
+        echo "<input type=\"submit\" name=\"valider\" value=\"Valider\"/>";
+    }
+    public function getform()
+    {
+        $page = file_get_contents("TestFormulaire.php");
+        echo '<pre>';
+        echo $page;
+        echo '</pre>';
+    }
+}
+
+
+$form = new Formulaire("Formulaire",post);
+$form->ajouterzonetexte(nom);
+$form->ajouterzonetexte(code);
+$form->ajouterbouton();
+$form->getform();
---
+++ TestFormulaire.php
@@ -0,0 +1,4 @@
+<html>
+<h1>Voici la page Test du Formulaire</h1>
+    <?php include ("Formulaire.php"); ?>
+</html>
